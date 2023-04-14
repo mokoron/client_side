d9cc81031087ffd6c8e3218db115bf5f29fdf98f
Yuliya Ru <mokoron@gmail.com>
2023-04-14 18:01:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,15 @@ faq = {
     "Вопрос 3": {"Ответ": "Ответ на вопрос 3", "Подсказка": "Подсказка для вопроса 3"},
 }
 
-selected_question = st.sidebar.selectbox("Выберите вопрос", list(faq.keys()))
+selected_question = None
 
 col1, col2, col3 = st.beta_columns(3)
 
 with col1:
     for question in faq:
-        expander = st.beta_expander(question)
+        expander = st.beta_expander(question, expanded=(question == selected_question))
+        if expander.clicked:
+            selected_question = question
         with expander:
             if question == selected_question:
                 answer = st.text_input("Введите ответ:")
@@ -25,14 +27,20 @@ with col1:
                 st.write(faq[question]["Ответ"])
 
 with col2:
-    st.write(f"Подсказка: {faq[selected_question]['Подсказка']}")
-    st.write("Варианты ответов:")
-    st.write("1. Ответ 1")
-    st.write("2. Ответ 2")
-    st.write("3. Ответ 3")
-    st.write("4. Ответ 4")
-    st.write("5. Ответ 5")
+    if selected_question is None:
+        st.write("Выберите вопрос в левой колонке.")
+    else:
+        st.write(f"Подсказка: {faq[selected_question]['Подсказка']}")
+        st.write("Варианты ответов:")
+        st.write("1. Ответ 1")
+        st.write("2. Ответ 2")
+        st.write("3. Ответ 3")
+        st.write("4. Ответ 4")
+        st.write("5. Ответ 5")
 
 with col3:
-    st.write(f"Ответ на вопрос '{selected_question}':")
-    st.write(faq[selected_question]["Ответ"])
+    if selected_question is None:
+        st.write("Выберите вопрос в левой колонке.")
+    else:
+        st.write(f"Ответ на вопрос '{selected_question}':")
+        st.write(faq[selected_question]["Ответ"])
