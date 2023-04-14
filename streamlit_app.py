668bcce38b962c6ae1d4d23bca56f9ad7a7a1f7e
Yuliya Ru <mokoron@gmail.com>
2023-04-14 18:09:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,32 +8,33 @@ faq = {
     "Вопрос 3": {"Ответ": "Ответ на вопрос 3", "Подсказка": "Подсказка для вопроса 3"},
 }
 
-selected_question = st.selectbox("Выберите вопрос", list(faq.keys()))
+if __name__ == '__main__':
+    st.set_page_config(layout="wide")
 
-st.set_page_config(layout="wide")
+    selected_question = st.selectbox("Выберите вопрос", list(faq.keys()))
 
-col1, col2, col3 = st.beta_columns(3)
+    col1, col2, col3 = st.beta_columns(3)
 
-with col1:
-    for question in faq:
-        expander = st.beta_expander(question)
-        with expander:
-            if question == selected_question:
-                answer = st.text_input("Введите ответ:")
-                if answer:
-                    faq[selected_question]["Ответ"] = answer
-            else:
-                st.write(faq[question]["Ответ"])
+    with col1:
+        for question in faq:
+            expander = st.beta_expander(question)
+            with expander:
+                if question == selected_question:
+                    answer = st.text_input("Введите ответ:")
+                    if answer:
+                        faq[selected_question]["Ответ"] = answer
+                else:
+                    st.write(faq[question]["Ответ"])
 
-with col2:
-    st.write(f"Подсказка: {faq[selected_question]['Подсказка']}")
-    st.write("Варианты ответов:")
-    st.write("1. Ответ 1")
-    st.write("2. Ответ 2")
-    st.write("3. Ответ 3")
-    st.write("4. Ответ 4")
-    st.write("5. Ответ 5")
+    with col2:
+        st.write(f"Подсказка: {faq[selected_question]['Подсказка']}")
+        st.write("Варианты ответов:")
+        st.write("1. Ответ 1")
+        st.write("2. Ответ 2")
+        st.write("3. Ответ 3")
+        st.write("4. Ответ 4")
+        st.write("5. Ответ 5")
 
-with col3:
-    st.write(f"Ответ на вопрос '{selected_question}':")
-    st.write(faq[selected_question]["Ответ"])
+    with col3:
+        st.write(f"Ответ на вопрос '{selected_question}':")
+        st.write(faq[selected_question]["Ответ"])
